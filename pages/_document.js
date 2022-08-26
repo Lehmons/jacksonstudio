8dd568f3c78acbffe50a0d38181ea18aff398af4
Lehmons <freddiejackson217@gmail.com>
2022-08-26 19:03:27 +0100

    updated meta

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { DocumentContext } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -21,5 +21,21 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1, minimum-scale=1, maximum-scale=1"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
